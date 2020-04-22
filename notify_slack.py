b9abae487ae9184a56c58fe6c1f2749dab0d93b8
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-22 10:29:23 -0400

    setup a new lambda function

--- notify_slack.py
+++ notify_slack.py
@@ -2,7 +2,7 @@ from urllib import request, parse
 import json
 
 def send_message():
-    url = 'https://hooks.slack.com/services/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'
+    url = 'https://hooks.slack.com/services/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'
     body = {"text": "Please dont delete this is a working in progress app"}
 
     jsondata = json.dumps(body);
