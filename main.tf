90439eb4b7ff8f6b95b7143d58a08f1dcb7f9da8
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-22 10:02:22 -0400

    attempting to upload a new lambda

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "archive" {}
 data "archive_file" "lambda" {
   type = "zip"
 
-  source_file = "hello.py"
-  output_path = "hello.zip"
+  source_file = "notify_slack.py"
+  output_path = "notify_slack.zip"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -28,15 +28,15 @@ EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "hello.zip"
+  filename      = "notify_slack.zip"
   function_name = "terraform-slack-notifier"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "hello.notify_slack"
+  handler       = "notify_slack.send_message"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("hello.zip")
+  source_code_hash = filebase64sha256("notify_slack.zip")
 
   runtime = "python3.7"
 
@@ -45,4 +45,6 @@ resource "aws_lambda_function" "test_lambda" {
       foo = "bar"
     }
   }
+
+  depends_on = ["data.archive_file.lambda"]
 }
--- notify_slack.py
+++ notify_slack.py
@@ -1,14 +1,19 @@
 from urllib import request, parse
 import json
 
-url = 'https://hooks.slack.com/services/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'
-body = {"text": "Hello, World!"}
+def send_message():
+    url = 'https://hooks.slack.com/services/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'
+    body = {"text": "Hello, World!"}
 
-jsondata = json.dumps(body);
-jsondatabytes = jsondata.encode('utf-8')
+    jsondata = json.dumps(body);
+    jsondatabytes = jsondata.encode('utf-8')
 
-req =  request.Request(url)
-req.add_header('Content-type', 'application/json');
-resp = request.urlopen(req, jsondatabytes)
+    req =  request.Request(url)
+    req.add_header('Content-type', 'application/json');
+    resp = request.urlopen(req, jsondatabytes)
 
-print(resp)
+    print(resp)
+
+
+
+send_message()
