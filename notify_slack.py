a3f5959740bd701e90e414fa17f1df6af571b293
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-22 09:47:12 -0400

    adding a clean standard libary only slack notifier

--- notify_slack.py
+++ notify_slack.py
@@ -1,24 +1,14 @@
 from urllib import request, parse
 import json
-body = {"text": "Hello, World!"}
 
-url = 'https://hooks.slack.com/services/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'
-url = 'https://hooks.slack.com/services/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'
-headers = {'Content-type': 'application/json'}
-data = parse.urlencode(body).encode()
+url = 'https://hooks.slack.com/services/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'
+body = {"text": "Hello, World!"}
 
 jsondata = json.dumps(body);
 jsondatabytes = jsondata.encode('utf-8')
 
-
-req =  request.Request(url, data=data) # this will make the method "POST"
+req =  request.Request(url)
 req.add_header('Content-type', 'application/json');
 resp = request.urlopen(req, jsondatabytes)
 
 print(resp)
-
-
-#import urllib.request
-
-#x = urllib.request.urlopen('https://www.google.com/')
-#print(x.read())
