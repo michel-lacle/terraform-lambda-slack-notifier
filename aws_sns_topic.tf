0d9065c725b3fef448badc2cf71cf34da3dc24bc
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-22 11:44:10 -0400

    create sns subscript for slack lambda

--- aws_sns_topic.tf
+++ aws_sns_topic.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "slack-notifier" {
   name = "slack-notifier-tf"
 }
+
+resource "aws_sns_topic_subscription" "slack-lambda-notification-subscription" {
+  topic_arn = aws_sns_topic.slack-notifier.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.test_lambda.arn
+}
