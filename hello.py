1d668d517cb8cc3328720ba9858aea41fd61e86f
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-21 21:51:49 -0400

    adding lambda function

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 import requests
 
-url = 'https://hooks.slack.com/services/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'
-myobj = '{"text":"Hello, World!"}'
-headers = { 'Content-type' : 'application/json'}
+def notify_slack(event, context):
+    url = 'https://hooks.slack.com/services/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'
+    myobj = '{"text":"Hello, World!"}'
+    headers = { 'Content-type' : 'application/json'}
 
-x = requests.post(url, data = myobj, headers = headers)
+    x = requests.post(url, data=myobj, headers=headers)
 
-print(x.text)
+    print(x.text)
+
+    return {
+        'message' : x.text
+    }
+
