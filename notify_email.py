3f651be87d6a8b202b940cc8e328661e5e175ab6
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-24 23:58:30 -0400

    add email lambda

--- notify_email.py
+++ notify_email.py
@@ -4,8 +4,7 @@ import json, os, boto3
 
 def send_message(event, context):
     # get url from environment variable
-    arn = os.environ['SLACK_URL']
-    s3url = os.environ['S3_DOWNLOAD_URL']
+    arn = os.environ['EMAIL_TOPIC_ARN']
 
     client = boto3.client('sns')
 
